9f99f8a2ff390467f728b21e7064ac0b431c149a
Kiran <kirans2494@gmail.com>
2018-10-07 18:15:55 +0530

    Added rental detail,rental service,rental list,rental model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes,RouterModule} from '@angular/router';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RentalModule } from './rental/rental.module';
+import { RentalComponent } from './rental/rental.component';
+import { CommonModule } from '@angular/common';
+
+const routes:Routes=[
+  {path:'',redirectTo:'/rentals',pathMatch:'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+
+   
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RentalModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
